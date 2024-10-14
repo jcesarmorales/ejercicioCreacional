d8426714463bf8fed0e29fe3c5e0fb93237cc811
Julio Morales <jcesarmorales@unicesar.edu.co>
2024-10-13 19:13:23 -0500

    commit inicial

---
+++ src/main/java/reportInformecom/example/reportInf/ReportInfApplication.java
@@ -0,0 +1,13 @@
+package reportInformecom.example.reportInf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportInfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportInfApplication.class, args);
+	}
+
+}
